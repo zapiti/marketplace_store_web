12f6ccfa366729d8c31974a9276c69ae7a2bbae6
Nathan Oliveira <nathan.oliveira@exa.com.br>
2025-05-24 20:22:45 -0300

    ajustest

--- android/app/src/main/kotlin/br/com/zapiti/marketplace_store_web/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package br.com.zapiti.marketplace_store_web
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/example/new_marketplace_web/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.new_marketplace_web
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
